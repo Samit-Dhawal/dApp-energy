40510e3ad8fb82187e634ad3a0db2b72758dcc53
Samit Dhawal <samitdhawal19@gnu.ac.in>
2022-03-19 19:25:25 +0530

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Header()
+{
+    return <div>
+        <h2>This is Header</h2>
+    </div>
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../components/Header';
+
+export default function Home()
+{
+    return <div>
+        <Header />
+        <h3> Hello Samit </h3>
+    </div>
+}
