4b765c82b5225a23e284705200f27231e0739225
harshkumar093 <55792055+harshkumar093@users.noreply.github.com>
2022-03-19 23:18:47 +0530

    login API-2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,20 +1,32 @@
 import React,{useState} from 'react';
+import Header from '../components/Header';
 
 export default function Login(){
     const [email,setEmail]=useState('')
     const [pass,setPass]=useState('')
-    const login=(evt)=>{
+    const login= async(evt)=>{
         evt.preventDefault();
         console.log(email,pass);
+        await fetch(`http://localhost:8001/readUser?email=${email}&password=${pass}`).then(res=>res.json()).then(async(data)=>{
+            console.log(data);
+            if(data.success){
+                console.log('loggin')
+                setEmail("");
+                setPass("");
+            }else{
+                console.log('login failed')
+            }
+        });
     }
     return <div>
-        <form style={{ textAlign: "center", padding: 100 }} method="GET" onSubmit={(evt)=>login(evt)}>
+        <Header/>
+        <form style={{ textAlign: "center", paddingTop: 100 }} onSubmit={(evt)=>login(evt)}>
             <label>
                 Username:<br />
-                <input type="text" value={email} onChange={(text)=>setEmail(text)} />
+                <input type="text" value={email} onChange={(text)=>setEmail(text.target.value)} />
                 <br />
                 Password:<br />
-                <input type="password" value={pass} onChange={(text)=>setPass(text)} />
+                <input type="password" value={pass} onChange={(text)=>setPass(text.target.value)} />
             </label>
             <br />
             <input type="submit" value="Submit" />
