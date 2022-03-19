464281985ec637c60de77eec147a73ffce6bd0b4
harshkumar093 <55792055+harshkumar093@users.noreply.github.com>
2022-03-19 20:20:08 +0530

    sale box added

---
+++ src/components/home-components/OnSale.js
@@ -0,0 +1,26 @@
+export default function OnSale({name,units,price}){
+    return(
+        <div style={styles.contianer}>
+            <div style={{display:'flex',flexDirection:'row',justifyContent:'space-between'}}>
+            <h2>Seller : {name}</h2><button>Buy</button>
+            </div>
+            <br/>
+            <hr/>
+            <br/>
+            <div style={{display:'flex',flexDirection:'row',justifyContent:'space-between'}}>
+            <h3>Units : {units}</h3><h3>Price/Unit : {price}</h3>
+            </div>
+        </div>
+    )
+}
+
+const styles = {
+    contianer:{
+        display:'inline-block',
+        border:'1px solid gray',
+        width:'28%',
+        padding:20,
+        fontFamily:'monospace',
+        margin:10
+    },
+}
