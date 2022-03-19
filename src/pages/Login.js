a6ec0120685f299dd1b2935319f1b477995481b8
harshkumar093 <55792055+harshkumar093@users.noreply.github.com>
2022-03-19 22:53:20 +0530

    login API-1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React,{useState} from 'react';
 
-export default function Login()
-{
+export default function Login(){
+    const [email,setEmail]=useState('')
+    const [pass,setPass]=useState('')
+    const login=(evt)=>{
+        evt.preventDefault();
+        console.log(email,pass);
+    }
     return <div>
-        <form style={{ textAlign: "center", padding: 100 }}>
+        <form style={{ textAlign: "center", padding: 100 }} method="GET" onSubmit={(evt)=>login(evt)}>
             <label>
                 Username:<br />
-                <input type="text" name="name" />
+                <input type="text" value={email} onChange={(text)=>setEmail(text)} />
                 <br />
                 Password:<br />
-                <input type="password" name="password" />
+                <input type="password" value={pass} onChange={(text)=>setPass(text)} />
             </label>
             <br />
             <input type="submit" value="Submit" />
