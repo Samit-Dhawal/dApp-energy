798d9ea78ac110fd273692f5a6159817477cbe05
Samit Dhawal <samitdhawal19@gnu.ac.in>
2022-03-21 15:48:52 +0530

    git pull fixes

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,91 +2,84 @@ import { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Holdings from '../components/profile-components/Holdings';
 import Transaction from '../components/profile-components/Transaction';
-
-<<<<<<< HEAD
 export default function Profile()
 {
     const [email, setEmail] = useState('');
     const [id, setId] = useState('');
     const [wallet, setWallet] = useState('');
+    const [form, setForm] = useState(false);
+    const [newUnits, setNewUnits] = useState('');
+    const [newPrice, setNewPrice] = useState('');
+    const [error, setError] = useState(false);
+    const [holdings, setHoldings] = useState([]);
+    const [transactions, setTransactions] = useState([]);
     const checkData = (x) =>
     {
         if (x === null || x === undefined || x === '') { return false }
         return true;
     }
-    useEffect(() =>
+    useEffect(async () =>
     {
         var id = localStorage.getItem('id');
         var email = localStorage.getItem('email');
         var wallet = localStorage.getItem('wallet');
         if (checkData(id) && checkData(email) && checkData(wallet))
         {
-            setId(id); setEmail(email); setWallet(wallet);
-        } else
-        {
-            window.location.href = "signin";
-=======
-export default function Profile(){
-    const [email,setEmail] = useState('');
-    const [id,setId]=useState('');
-    const [wallet,setWallet]=useState('');
-    const [form,setForm]=useState(false);
-    const [newUnits,setNewUnits]=useState('');
-    const [newPrice,setNewPrice]=useState('');
-    const [error,setError]=useState(false);
-    const [holdings,setHoldings] = useState([]);
-    const [transactions,setTransactions] = useState([]);
-    const checkData=(x)=>{
-        if(x===null||x===undefined||x===''){return false}
-        return true;
-    }
-    useEffect(async()=>{
-        var id = localStorage.getItem('id');
-        var email = localStorage.getItem('email');
-        var wallet = localStorage.getItem('wallet');
-        if(checkData(id)&&checkData(email)&&checkData(wallet)){
             setId(id);
             setEmail(email);
             setWallet(wallet);
             await getHoldings();
             await getTransactions();
-        }else{
-            window.location.href="signin";
->>>>>>> cfd619f659b89d04b63b79d868a4652c8b7b42d0
+        } else
+        {
+            window.location.href = "signin";
         }
-    },[])
-    const openForm =()=>{
+    })
+    const openForm = () =>
+    {
         setForm(true);
     }
-    const saveHolding=async()=>{
-        await fetch(`https://dapp-energy.herokuapp.com/createHolding?email=${email}&id=${id}&units=${newUnits}&price=${newPrice}`).then(res=>res.json()).then(data=>{
-            if(data.success){
+    const saveHolding = async () =>
+    {
+        await fetch(`https://dapp-energy.herokuapp.com/createHolding?email=${ email }&id=${ id }&units=${ newUnits }&price=${ newPrice }`).then(res => res.json()).then(data =>
+        {
+            if (data.success)
+            {
                 setForm(false);
                 setError(false);
                 getHoldings();
-            }else{
+            } else
+            {
                 setError(true);
             }
         })
     }
-    const getHoldings=async()=>{
-        await fetch(`https://dapp-energy.herokuapp.com/readHoldingById?id=${id}`).then(res=>res.json()).then(data=>{
-            if(data.success){
+    const getHoldings = async () =>
+    {
+        await fetch(`https://dapp-energy.herokuapp.com/readHoldingById?id=${ id }`).then(res => res.json()).then(data =>
+        {
+            if (data.success)
+            {
                 setHoldings(data.data)
                 console.log('holding')
                 setError(false);
-            }else{
+            } else
+            {
                 setError(true);
             }
         })
     }
-    const getTransactions=async()=>{
-        await fetch(`https://dapp-energy.herokuapp.com/readTransactions`).then(res=>res.json()).then(data=>{
-            if(data.success){
+    const getTransactions = async () =>
+    {
+        await fetch(`https://dapp-energy.herokuapp.com/readTransactions`).then(res => res.json()).then(data =>
+        {
+            if (data.success)
+            {
                 setTransactions(data.data);
                 console.log('transactions')
                 setError(false);
-            }else{
+            } else
+            {
                 setError(true);
             }
         })
@@ -103,13 +96,13 @@ export default function Profile(){
             </div>
             <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', padding: 40 }}>
                 <h1 style={{ fontFamily: 'monospace' }}>Your Holdings</h1>
-                {form?(<div><input type="text" placeholder='Enter Units' value={newUnits} onChange={(evt=>{setNewUnits(evt.target.value)})}/>{"  "}<input type="text" placeholder='Enter price/unit' value={newPrice} onChange={(evt=>{setNewPrice(evt.target.value)})}/>{"  "}<button onClick={()=>{saveHolding()}}>+</button></div>):(
-                <button onClick={()=>openForm()}>Add new holding +</button>
+                {form ? (<div><input type="text" placeholder='Enter Units' value={newUnits} onChange={(evt => { setNewUnits(evt.target.value) })} />{"  "}<input type="text" placeholder='Enter price/unit' value={newPrice} onChange={(evt => { setNewPrice(evt.target.value) })} />{"  "}<button onClick={() => { saveHolding() }}>+</button></div>) : (
+                    <button onClick={() => openForm()}>Add new holding +</button>
                 )}
             </div>
-                    
+
             <div style={{ paddingLeft: 60, paddingRight: 60 }}>
-            {error?(<><label style={{color:'red',fontWeight:800,fontSize:20}}>Error: Not able to add holding, try again later</label><br/></>):(<></>)}
+                {error ? (<><label style={{ color: 'red', fontWeight: 800, fontSize: 20 }}>Error: Not able to add holding, try again later</label><br /></>) : (<></>)}
                 {holdings.length === 0 ? (
                     <h2>Your current holding is empty, add new holding to sell electricity</h2>
                 ) : (<>
