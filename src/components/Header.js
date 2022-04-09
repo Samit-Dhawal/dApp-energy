50a853d767a977c14d7996094dc0df8c57fc4b7c
Samit Dhawal <samitdhawal19@gnu.ac.in>
2022-04-09 12:05:23 +0530

    Creted Admin Dashboard

--- src/components/Header.js
+++ src/components/Header.js
@@ -46,7 +46,6 @@ export default function Header()
                             Sign In
                         </a>
                     </>)}
-
             </div>
         </div>
     );
--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,23 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 
+const server = "http://localhost:8001/";
 
 export default function Admin()
 {
 
     return <div>
         <Header />
-        <div style={styles.alertTran}>
-            <div style={styles.pendApp}>Pending</div>
-            <div>Approved</div>
+        <div class="m-3 row">
 
+            <div class="col-6">
+                <div class="col-6 p-3 h3 text-center col-md-12"><button type='button' class="btn-warning " data-bs-toggle="collapse" data-bs-target="#pending"> Pending</button></div>
+                <div id='pending' class="col-12 bg-warning text-black p-4 col-md-12">
+
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="col-6 p-3 h3 text-center col-md-12"><button type='button' class="btn-success" data-bs-toggle="collapse" data-bs-target="#approved"> Approved</button></div>
+                <div id='approved' class="col-12 bg-success text-white p-4 col-md-12">
+
+                </div>
+            </div>
         </div>
-    </div>
-}
+        <div>
+            <div class="">
+                <div class=" p-3 h3 text-center "><button type='button' class="btn-info " data-bs-toggle="collapse" data-bs-target="#allTransac"> Transactions</button></div>
+                <div id='allTransac' class=" bg-info text-black p-4 mx-5">
 
+                </div>
+            </div>
+        </div>
+    </div >
+
+}
 
 const styles = {
     alertTran: {
         display: "flex"
-    },
+    }
 }
